ef0f23fba4cdc85cffedb9de32b6b9dbd37f7157
ehw014 <35472009+ehw014@users.noreply.github.com>
2022-11-28 10:01:45 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -4,6 +4,32 @@ rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
 
+SCORE="0"
 
+if [ -f ListExamples.java]
+then
+    echo "ListExamples.java exists"
+else
+    echo "ListExamples.java not found, Score = "$SCORE
+    exit
+fi 
 
+cp ../TestListExamples.java ./
 
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+#javac -cp *.java
+
+if [ $? -eq 0]
+then
+    echo "Successsfully Compiled"
+else
+    echo "Unsuccessful Compilation, Score = "$SCORE
+fi
+#java -cp TestListExamples > testresults.txt
+java -cp .:lib/hamcrest-core-1.3.jar:lib/Junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > TestResults.txt
+
+if [ $? -eq 0]
+then
+    echo "Successfully Run"
+else
+    echo "Runtime Error, Score = "$SCORE
